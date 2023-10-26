0ba684c1efe5df9491b1d2fbbde61ce15d329ed7
Sagar Dighe <sagasoftech@gmail.com>
2023-10-26 00:32:54 -0500

    Initial project structure

---
+++ src/test/java/com/sagasoftech/springboot/SpringBootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
